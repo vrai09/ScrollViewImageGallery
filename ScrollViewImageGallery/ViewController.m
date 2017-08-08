fced01114a9e79e37a8c1482a804817d6de18d78
Varninder Rai <varninder@openmailbox.org>
2017-08-07 21:15:34 -0700

    Image gallery

--- ScrollViewImageGallery/ViewController.m
+++ ScrollViewImageGallery/ViewController.m
@@ -8,7 +8,10 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIScrollViewDelegate>
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIPageControl *pageControl;
+
 
 @end
 
@@ -16,13 +19,56 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    _scrollView.delegate = self;
+    _scrollView.minimumZoomScale = 0.5;
+    _scrollView.maximumZoomScale = 2.0;
+    _scrollView.pagingEnabled = YES;
+    
+    
+    UIImageView *image1 = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"Image-1"]];
+    UIImageView *image2 = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"Image"]];
+    UIImageView *image3 = [[UIImageView alloc]initWithImage:[UIImage imageNamed:@"Lighthouse-in-Field"]];
+    NSArray *images = @[image1, image2, image3];
+    
+    
+    //int i = 0;
+    for(UIImageView *imageView in images) {
+        /*imageView.frame = CGRectMake(i*_scrollView.frame.size.width, 0, _scrollView.frame.size.width, _scrollView.frame.size.height); */
+        
+        imageView.contentMode = UIViewContentModeScaleAspectFit;
+
+       // [_scrollView addSubview:imageView];
+        
+        //i++;
+    }
+    
+    UIStackView *stackview = [[UIStackView alloc]initWithArrangedSubviews:images];
+    stackview.axis = UILayoutConstraintAxisHorizontal;
+    stackview.distribution = UIStackViewDistributionFillEqually;
+    [_scrollView addSubview:stackview];
+    
+    stackview.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    [stackview.heightAnchor constraintEqualToAnchor:_scrollView.heightAnchor multiplier:1].active = YES;
+    [stackview.widthAnchor constraintEqualToAnchor:_scrollView.widthAnchor multiplier:images.count].active = YES;
+    
+    [stackview.topAnchor constraintEqualToAnchor:_scrollView.topAnchor].active = YES;
+    [stackview.bottomAnchor constraintEqualToAnchor:_scrollView.bottomAnchor].active = YES;
+    [stackview.leadingAnchor constraintEqualToAnchor:_scrollView.leadingAnchor].active = YES;
+    [stackview.trailingAnchor constraintEqualToAnchor:_scrollView.trailingAnchor].active = YES;
+    
+    _pageControl.numberOfPages = images.count;
+        
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    
+    CGFloat pageWidth = _scrollView.frame.size.width;
+    float fractionalPage = self.scrollView.contentOffset.x / pageWidth;
+    NSInteger page = lround(fractionalPage);
+    self.pageControl.currentPage = page;
 }
 
 
